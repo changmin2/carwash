c40b06283b3b2fd5eb3500440fe8b24af1b0e733
rsy0921 <rsy0921@naver.com>
2023-11-28 12:10:53 +0900

    날씨 관련 변경

--- lib/common/component/weather.dart
+++ lib/common/component/weather.dart
@@ -1,77 +1,129 @@
+import 'package:carwash/common/const/sizes.dart';
 import 'package:carwash/weather/model/weather_model.dart';
 import 'package:flutter/material.dart';
 
 class Weather extends StatelessWidget {
   final WeatherInfo weatherInfo;
 
-  const Weather({
-    required this.weatherInfo,
-    Key? key
-  }) : super(key: key);
+  const Weather({required this.weatherInfo, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     final day = weatherInfo.dt_txt.split(" ")[0].split("-");
     final time = weatherInfo.dt_txt.split(" ")[1].split(":");
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30.0),
-        side: BorderSide(color: Colors.black,width: 0.5)
+
+    return Container(
+      width: 70,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        border: Border.all(color: Colors.grey),
       ),
-      child: SizedBox(
-        width: 100,
-        height: 180,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-                day[1]+'/'+day[2],
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500
-                ),
-            ),
-            Text(
-              time[0]+'시',
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500
-              ),
-            ),
-            SizedBox(height: 16),
-            weatherInfo.weather[0].main == 'Clear' ?
-            Icon(
-              Icons.sunny,
-              color: Colors.red,
-            )
-            : weatherInfo.weather[0].main == 'Clouds' ?
-            Icon(
-              Icons.cloud,
-              color: Colors.blue,
-            ) : Icon(
-              Icons.cloudy_snowing,
-              color: Colors.blue,
-            ),
-            SizedBox(height: 16),
-            Text(
-              weatherInfo.main.humidity.toString()+'%',
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500
-              ),
-            ),
-            SizedBox(height: 16,),
-            Text(
-              (weatherInfo.main.temp-273.15).floor().toString()+' 도',
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500
-              ),
-            )
-          ],
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            day[1] + '/' + day[2],
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+
+          const SizedBox(height: TSizes.xs),
+
+          Text(
+            '${time[0]}시',
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+
+          const SizedBox(height: TSizes.xs / 2),
+
+          weatherInfo.weather[0].main == 'Clear'
+              ? const Icon(
+                  Icons.sunny,
+                  color: Colors.red,
+                  size: 25,
+                )
+              : weatherInfo.weather[0].main == 'Clouds'
+                  ? const Icon(
+                      Icons.cloud,
+                      color: Colors.grey,
+                      size: 25,
+                    )
+                  : const Icon(
+                      Icons.cloudy_snowing,
+                      color: Colors.blue,
+                      size: 25,
+                    ),
+
+          const SizedBox(height: TSizes.xs),
+
+          Text(
+            '${weatherInfo.main.humidity}%',
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+
+          const SizedBox(height: TSizes.xs / 2),
+
+          Text(
+            '${(weatherInfo.main.temp - 273.15).floor()}℃',
+            style: Theme.of(context).textTheme.labelLarge,
+          )
+        ],
       ),
     );
+
+    // return Card(
+    //   shape: RoundedRectangleBorder(
+    //     borderRadius: BorderRadius.circular(20.0),
+    //     side: const BorderSide(
+    //       color: Colors.grey,
+    //       width: 0.5,
+    //     ),
+    //   ),
+    //   child: SizedBox(
+    //     width: 70,
+    //     height: 110,
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         Text(
+    //           day[1] + '/' + day[2],
+    //           style: Theme.of(context).textTheme.labelLarge,
+    //         ),
+    //         SizedBox(height: 4),
+    //         Text(
+    //           time[0] + '시',
+    //           style: Theme.of(context).textTheme.labelLarge,
+    //         ),
+    //         SizedBox(height: 8),
+    //         weatherInfo.weather[0].main == 'Clear'
+    //             ? const Icon(
+    //                 Icons.sunny,
+    //                 color: Colors.red,
+    //                 size: 25,
+    //               )
+    //             : weatherInfo.weather[0].main == 'Clouds'
+    //                 ? const Icon(
+    //                     Icons.cloud,
+    //                     color: Colors.grey,
+    //                     size: 25,
+    //                   )
+    //                 : const Icon(
+    //                     Icons.cloudy_snowing,
+    //                     color: Colors.blue,
+    //                     size: 25,
+    //                   ),
+    //         SizedBox(height: 8),
+    //         Text(
+    //           weatherInfo.main.humidity.toString() + '%',
+    //           style: Theme.of(context).textTheme.labelLarge,
+    //         ),
+    //         SizedBox(height: 4),
+    //         Text(
+    //           (weatherInfo.main.temp - 273.15).floor().toString() + ' 도',
+    //           style: Theme.of(context).textTheme.labelLarge,
+    //         )
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
