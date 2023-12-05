4c52037b372dae561f32961f4d283e9686053b40
rsy0921 <rsy0921@naver.com>
2023-12-06 06:13:13 +0900

    라운드컨테이너 추가

---
+++ lib/common/component/rounded_container.dart
@@ -0,0 +1,45 @@
+import 'package:carwash/common/const/sizes.dart';
+import 'package:flutter/material.dart';
+
+class TRoundedContainer extends StatelessWidget {
+
+  final double? width;
+  final double? height;
+  final double radius;
+  final Widget? child;
+  final bool showBorder;
+  final Color borderColor;
+  final Color backgroundColor;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+
+
+  const TRoundedContainer({
+    super.key,
+    this.width,
+    this.height,
+    this.radius = TSizes.cardRadiusLg,
+    this.child,
+    this.showBorder = false,
+    this.borderColor = Colors.grey,
+    this.backgroundColor = Colors.white,
+    this.padding,
+    this.margin,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: padding,
+      margin: margin,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(radius),
+        border: showBorder ? Border.all(color: borderColor) : null,
+      ),
+      child: child,
+    );
+  }
+}
