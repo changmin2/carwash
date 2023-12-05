1ff3af3d20859ab2561baa1fb1b03d511fad64e4
rsy0921 <rsy0921@naver.com>
2023-12-06 08:37:48 +0900

    세차 기록 상세화면 변경

--- lib/car/view/record_detail_screen.dart
+++ lib/car/view/record_detail_screen.dart
@@ -1,6 +1,7 @@
-
 import 'package:carwash/car/component/washlist_card.dart';
 import 'package:carwash/car/provider/record_provider.dart';
+import 'package:carwash/common/component/rounded_container.dart';
+import 'package:carwash/common/const/sizes.dart';
 import 'package:carwash/common/layout/default_layout_v2.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
@@ -10,9 +11,7 @@ import 'package:intl/intl.dart';
 class RecordDetail extends ConsumerStatefulWidget {
   static get routeName => 'recordDetailScreen';
   final int id;
-  const RecordDetail({
-    required this.id,
-    Key? key}) : super(key: key);
+  const RecordDetail({required this.id, Key? key}) : super(key: key);
 
   @override
   ConsumerState<RecordDetail> createState() => _RecordDetailState();
@@ -23,123 +22,100 @@ class _RecordDetailState extends ConsumerState<RecordDetail> {
   Widget build(BuildContext context) {
     final state = ref.read(RecordProvider('false')).firstWhere((element) => element.id == widget.id);
     var wash = state.washList.split("[")[1].split("]")[0].split(",");
+
     return DefaultLayoutV2(
-      appBar: _renderAppbar(context),
+      appBar: AppBar(),
       child: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.only(left: 20,right: 20),
+          padding: const EdgeInsets.all(TSizes.defalutSpace),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 40),
+              /// 세차일자
               Text(
-                '세차  일자',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700
-                ),
+                '세차 일자',
+                style: Theme.of(context).textTheme.titleMedium,
               ),
-              const SizedBox(height: 16),
-              Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    side: BorderSide(width: 0.4)
-                ),
+
+              const SizedBox(height: TSizes.spaceBtwInputFields),
+
+              TRoundedContainer(
+                showBorder: true,
                 child: ListTile(
                   title: Text(
-                      DateFormat('yyyy년 MM월 dd일 ').format(state.date)
-                          +'('+ DateFormat('E','ko').format(state.date)+')',
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400
-                    ),
+                    '${DateFormat('yyyy년 MM월 dd일 ').format(state.date)}(${DateFormat('E', 'ko').format(state.date)})',
+                    style: Theme.of(context).textTheme.bodyMedium,
                   ),
                 ),
               ),
-              const SizedBox(height: 8,),
+
+              const SizedBox(height: TSizes.spaceBtwItems),
+              
+              /// 세차 장소
               Text(
-                '세차  장소',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700
+                '세차 장소',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+
+              const SizedBox(height: TSizes.spaceBtwInputFields),
+
+              TRoundedContainer(
+                showBorder: true,
+                child: ListTile(
+                  title: Text(
+                    state.place,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
                 ),
               ),
-              const SizedBox(height: 8,),
-              Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      side: BorderSide(width: 0.4)
-                  ),
-                  child: ListTile(
-                    title: Text(
-                      state.place,
-                      style: TextStyle(
-                          decorationThickness: 0
-                      ),
-                    ),
-                  )
-              ),
-              const SizedBox(height: 16,),
+
+              const SizedBox(height: TSizes.spaceBtwItems),
+              
+              /// 사진
               Text(
                 '사진',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700
-                ),
+                style: Theme.of(context).textTheme.titleMedium,
               ),
-              const SizedBox(height: 16,),
+
+              const SizedBox(height: TSizes.spaceBtwInputFields),
+
               DottedBorder(
                 color: Colors.grey,
-                dashPattern: [5,3],
+                dashPattern: const [5, 3],
                 borderType: BorderType.RRect,
-                radius: Radius.circular(10),
-                child: Container(
-                    width: 400,
-                    height: 250,
-                    child: Image(
-                      image: NetworkImage(
-                        state.imgUrl
-                      ),
-                    )
-                )
+                radius: const Radius.circular(12),
+                child: Image(
+                  image: NetworkImage(state.imgUrl),
+                  width: 400,
+                  height: 250,
+                ),
               ),
-              const SizedBox(height: 16,),
+              
+              const SizedBox(height: TSizes.spaceBtwInputFields),
+              
+              /// 세차 순서
               Text(
                 '세차 순서',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+
+              const SizedBox(height: TSizes.spaceBtwInputFields),
+
+              Center(
+                child: Wrap(
+                  direction: Axis.horizontal,
+                  spacing: 3,
+                  runSpacing: 3,
+                  alignment: WrapAlignment.start,
+                  children: List.generate(wash.length, (index) {
+                    return WashListCard(step: index + 1, wash: wash[index]);
+                  }).toList(),
                 ),
               ),
-              const SizedBox(height: 16,),
-              Wrap(
-                direction: Axis.horizontal,
-                spacing: 5,
-                runSpacing: 5,
-                alignment: WrapAlignment.start,
-                children:
-                  List.generate(wash.length, (index) {
-                    return WashListCard(step: index+1, wash: wash[index]);
-                  }).toList()
-                ),
-              const SizedBox(height: 50)
             ],
           ),
         ),
-      )
+      ),
     );
   }
-}
-
-AppBar _renderAppbar(BuildContext context){
-  return AppBar(
-      backgroundColor: Colors.white,
-      centerTitle: true,
-      leading: IconButton(
-        onPressed: (){Navigator.pop(context);},
-        icon: Icon(Icons.arrow_back),
-        color: Colors.brown,
-        alignment: Alignment.centerLeft,
-      )
-  );
 }
