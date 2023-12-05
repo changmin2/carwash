16e7737710c1883f30488be882c78c2805b5630c
rsy0921 <rsy0921@naver.com>
2023-12-06 08:42:28 +0900

    최근 세차 카드 변경

--- lib/common/component/recent_carwash_card.dart
+++ lib/common/component/recent_carwash_card.dart
@@ -1,6 +1,5 @@
-import 'package:carwash/car/view/record_detail_screen.dart';
+import 'package:carwash/common/component/rounded_container.dart';
 import 'package:carwash/common/const/sizes.dart';
-import 'package:carwash/common/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class RecentCarWashCard extends StatelessWidget {
@@ -9,92 +8,47 @@ class RecentCarWashCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      // onTap: () => THelperFunctions.navigateToScreen(context, const RecordDetail(id: 2)),
-      child: SizedBox(
+      // onTap: () => THelperFunctions.navigateToScreen(context, const CarWashRecordScreen()),
+
+      child: TRoundedContainer(
         height: 80,
-        child: Padding(
-            padding: const EdgeInsets.all(TSizes.md),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: const EdgeInsets.all(TSizes.md),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
               children: [
-                Row(
+                
+                /// 리스트 이미지
+                Image.asset(
+                  'asset/img/login.png',
+                  width: 50,
+                  height: 50,
+                ),
+                
+                const SizedBox(width: TSizes.spaceBtwItems),
+                
+                /// 최근세차 정보
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Image.asset(
-                      'asset/img/login.png',
-                      width: 50,
-                      height: 50,
+                    Text(
+                      '2023-05-04',
+                      style: Theme.of(context).textTheme.bodyMedium,
                     ),
-                    const SizedBox(width: TSizes.spaceBtwItems),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '2023-05-04',
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        Text(
-                          '화곡동 카워시',
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        )
-                      ],
-                    ),
+                    Text(
+                      '화곡동 카워시',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    )
                   ],
                 ),
-                const Icon(Icons.keyboard_arrow_right),
-
               ],
             ),
+            const Icon(Icons.keyboard_arrow_right),
+          ],
         ),
       ),
     );
-
-
-    //   SizedBox(
-    //   width: double.infinity,
-    //   height: 100,
-    //   child: Card(
-    //     color: Colors.white,
-    //     elevation: 1.0,
-    //     shape: RoundedRectangleBorder(
-    //       borderRadius: BorderRadius.circular(12.0),
-    //     ),
-    //     child: Padding(
-    //       padding: const EdgeInsets.all(TSizes.md),
-    //       child: Row(
-    //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //         children: [
-    //           Row(
-    //             children: [
-    //               Image.asset(
-    //                 'asset/img/login.png',
-    //                 width: 50,
-    //                 height: 50,
-    //               ),
-    //               const SizedBox(width: TSizes.spaceBtwItems),
-    //               Column(
-    //                 mainAxisAlignment: MainAxisAlignment.center,
-    //                 crossAxisAlignment: CrossAxisAlignment.start,
-    //                 children: [
-    //                   Text(
-    //                     '2023-05-04',
-    //                     style: Theme.of(context).textTheme.bodyMedium,
-    //                   ),
-    //                   Text(
-    //                     '화곡동 카워시',
-    //                     style: Theme.of(context).textTheme.bodyMedium,
-    //                   )
-    //                 ],
-    //               ),
-    //             ],
-    //           ),
-    //           const Icon(Icons.keyboard_arrow_right),
-    //         ],
-    //       ),
-    //     ),
-    //   ),
-    // );
-
-
   }
 }
