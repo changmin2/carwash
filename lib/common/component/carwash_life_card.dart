3873e0a694f4293d18a98c77fb10e3a024905aab
rsy0921 <rsy0921@naver.com>
2023-12-06 19:47:09 +0900

    세차라이프 카드 변경

--- lib/common/component/carwash_life_card.dart
+++ lib/common/component/carwash_life_card.dart
@@ -1,3 +1,4 @@
+import 'package:carwash/common/component/rounded_container.dart';
 import 'package:carwash/common/const/sizes.dart';
 import 'package:carwash/common/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
@@ -11,146 +12,80 @@ class CarWashLife extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {},
-      child: Container(
+      child: TRoundedContainer(
         width: 230,
         height: 320,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12.0),
-            border: Border.all(color: Colors.grey.withOpacity(0.3))),
-        child: Padding(
-          padding: const EdgeInsets.all(TSizes.sm),
-          child: Column(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(12.0),
-                child: Image.asset(
-                  'asset/img/car_image.jpeg',
-                  fit: BoxFit.fill,
-                  height: 200,
-                ),
+        radius: 12.0,
+        showBorder: true,
+        padding: const EdgeInsets.all(TSizes.sm),
+        child: Column(
+          children: [
+            /// 사진
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Image.asset(
+                'asset/img/car_image.jpeg',
+                fit: BoxFit.fill,
+                height: 200,
               ),
-              const SizedBox(height: TSizes.md),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: TSizes.sm),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            const Icon(Icons.face),
-                            const SizedBox(width: TSizes.xs),
-                            Text(
-                              '세린이',
-                              style: Theme.of(context).textTheme.titleMedium,
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            const Icon(
-                              Icons.favorite,
-                              color: Colors.red,
-                            ),
-                            const SizedBox(width: TSizes.xs),
-                            Text(
-                              '120',
-                              style: Theme.of(context).textTheme.titleMedium,
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                    const SizedBox(height: TSizes.md),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          THelperFunctions.truncateText(
-                              '오늘 세차 맛집 다녀 왔습니다! 너무 깨끗했고 시설도 좋았습니다.', 15),
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+            ),
+
+            const SizedBox(height: TSizes.md),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: TSizes.sm),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      /// 닉네임
+                      Row(
+                        children: [
+                          const Icon(Icons.face),
+                          const SizedBox(width: TSizes.xs),
+                          Text(
+                            '세린이',
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                        ],
+                      ),
+
+                      /// 좋아요 갯수
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                          const SizedBox(width: TSizes.xs),
+                          Text(
+                            '120',
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+
+                  const SizedBox(height: TSizes.md),
+
+                  /// 글 내용
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        THelperFunctions.truncateText('오늘 세차 맛집 다녀 왔습니다! 너무 깨끗했고 시설도 좋았습니다.', 15),
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
-
-    //   SizedBox(
-    //   width: 230,
-    //   child: Card(
-    //     color: Colors.white,
-    //     elevation: 1.0,
-    //     shape: RoundedRectangleBorder(
-    //       borderRadius: BorderRadius.circular(12.0),
-    //     ),
-    //     child: Column(
-    //       children: [
-    //         ClipRRect(
-    //           borderRadius: const BorderRadius.vertical(
-    //             top: Radius.circular(12.0),
-    //           ),
-    //           child: Image.asset(
-    //             'asset/img/car_image.jpeg',
-    //             fit: BoxFit.fill,
-    //             height: 200,
-    //           ),
-    //         ),
-    //         const SizedBox(height: TSizes.md),
-    //         Padding(
-    //           padding: const EdgeInsets.symmetric(horizontal: TSizes.sm),
-    //           child: Column(
-    //             children: [
-    //               Row(
-    //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //                 children: [
-    //                   Row(
-    //                     children: [
-    //                       const Icon(Icons.face),
-    //                       const SizedBox(width: TSizes.xs),
-    //                       Text(
-    //                         '세린이',
-    //                         style: Theme.of(context).textTheme.titleMedium,
-    //                       ),
-    //                     ],
-    //                   ),
-    //                   Row(
-    //                     children: [
-    //                       const Icon(
-    //                         Icons.favorite,
-    //                         color: Colors.red,
-    //                       ),
-    //                       const SizedBox(width: TSizes.xs),
-    //                       Text(
-    //                         '120',
-    //                         style: Theme.of(context).textTheme.titleMedium,
-    //                       ),
-    //                     ],
-    //                   )
-    //                 ],
-    //               ),
-    //               const SizedBox(height: TSizes.sm),
-    //               Row(
-    //                 mainAxisAlignment: MainAxisAlignment.start,
-    //                 children: [
-    //                   Text(
-    //                     THelperFunctions.truncateText('오늘 세차 맛집 다녀 왔습니다! 너무 깨끗했고 시설도 좋았습니다.', 15),
-    //                     style: Theme.of(context).textTheme.bodyMedium,
-    //                   ),
-    //                 ],
-    //               ),
-    //             ],
-    //           ),
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
   }
 }
