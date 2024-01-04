9399582757c38eadac817cf707411e4669ca837c
rsy0921 <rsy0921@naver.com>
2024-01-04 20:17:46 +0900

    탭바

---
+++ lib/common/widgets/tabbar.dart
@@ -0,0 +1,30 @@
+import 'package:carwash/common/const/colors.dart';
+import 'package:carwash/common/utils/device/device_utility.dart';
+import 'package:flutter/material.dart';
+
+class TTabBar extends StatelessWidget implements PreferredSizeWidget {
+  final List<Widget> tabs;
+  final ValueChanged onTap;
+
+  const TTabBar({
+    super.key,
+    required this.tabs,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      tabs: tabs,
+      isScrollable: true,
+      indicatorColor: PRIMARY_COLOR,
+      unselectedLabelColor: Colors.grey,
+      labelColor: PRIMARY_COLOR,
+      onTap: onTap,
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(TDeviceUtils.getAppBarHeight());
+}
