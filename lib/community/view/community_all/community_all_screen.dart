2fa53fb9e554caedeb49b87744ef3773f026be3f
rsy0921 <rsy0921@naver.com>
2024-01-11 05:57:58 +0900

    커뮤니티 전체 구조 변경

--- lib/community/view/community_all/community_all_screen.dart
+++ lib/community/view/community_all/community_all_screen.dart
@@ -1,16 +1,14 @@
 import 'package:carwash/common/const/sizes.dart';
+import 'package:carwash/common/utils/helpers/helper_functions.dart';
 import 'package:carwash/community/provider/hot_all_community_provider.dart';
 import 'package:carwash/community/view/community_all/widget/all_board_hot_card_widget.dart';
 import 'package:carwash/community/view/community_all/widget/all_board_latest_post_widget.dart';
 import 'package:carwash/community/view/widget/community_section_heading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../common/view/pagination_list_view.dart';
-import '../../component/community_card.dart';
 import '../../provider/communityProvider.dart';
-import '../community_detail_screen.dart';
 import '../community_recent_screen.dart';
 
 class TCommunityAllScreen extends ConsumerWidget {
@@ -19,105 +17,76 @@ class TCommunityAllScreen extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final hot = ref.watch(hotAllCommunityProvider);
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(TSizes.defalutSpace),
-        child: Column(
-          children: [
-            Container(
-              height: 500,
-              child: Column(
-                children:[
-                  Expanded(
-                  child: PaginationListView(
-                      provider: communityProvider,
-                      itemBuilder: <CommunityModel>(_,index,community){
-                        if(index == 0){
-                          return Column(children: [
-                            /// HOT 전체
-                            /// HOT section header
-                            const TCommunitySectionHeading(
-                              firstTitle: 'HOT',
-                              firstTitleColor: Colors.red,
-                              secondTitle: '전체',
-                              // showActionButton: true,
-                              // buttonTitle: 'See All',
-                            ),
+    return Padding(
+      padding: const EdgeInsets.all(TSizes.defalutSpace),
+      child: PaginationListView(
+          provider: communityProvider,
+          itemBuilder: <CommunityModel>(_, index, community) {
+            if (index == 0) {
+              return Column(
+                children: [
+                  /// HOT 전체
+                  /// HOT section header
+                  const TCommunitySectionHeading(
+                    firstTitle: 'HOT',
+                    firstTitleColor: Colors.red,
+                    secondTitle: '전체',
+                    // showActionButton: true,
+                    // buttonTitle: 'See All',
+                  ),
 
-                            const SizedBox(height: TSizes.spaceBtwItems),
+                  const SizedBox(height: TSizes.spaceBtwItems),
 
-                            /// HOT 전체 리스트
-                            SizedBox(
-                              height: 270,
-                              child: ListView.builder(
-                                itemCount: hot.length,
-                                scrollDirection: Axis.horizontal,
-                                itemBuilder: (BuildContext context, int index) {
-                                  return Padding(
-                                    padding: EdgeInsets.only(right: TSizes.sm),
-                                    child: GestureDetector(
-                                      onTap: (){
-                                        Navigator.push(context, MaterialPageRoute(builder: (context) => CommunityRecentScreen(model: hot[index])));
-                                      },
-                                      child: TCommunityAllHotCardWidget(
-                                        containerWidth: 200,
-                                        imageUrl: 'asset/img/car_image.jpeg',
-                                        imageHeight: 200,
-                                        nickName: hot[index].creator,
-                                        title: hot[index].content,
-                                        likeCount: hot[index].favorite.toString(),
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ),
-                            ),
-
-                            const SizedBox(height: TSizes.spaceBtwItems),
-
-                            // SizedBox(
-                            //   width: double.infinity,
-                            //   child: OutlinedButton(
-                            //     onPressed: () {},
-                            //     style: ElevatedButton.styleFrom(
-                            //       backgroundColor: const Color(0xffE6EAEE),
-                            //     ),
-                            //     child: Text(
-                            //       "전체보기",
-                            //       style: Theme.of(context).textTheme.titleMedium
-                            //     ),
-                            //   ),
-                            // ),
-
-                            const SizedBox(height: TSizes.spaceBtwSections),
-
-                            /// 최신글
-                            /// 최신글 section heading
-                            const TCommunitySectionHeading(
-                              firstTitle: '최신글',
-                              secondTitle: '전체',
-                            ),
-
-                            const SizedBox(height: TSizes.spaceBtwItems),
-
-                            TCommunityAllLatestPostWidget(
-                              model: community,
-                            )
-                          ],
-                          );
-                        }
-                        return TCommunityAllLatestPostWidget(
-                          model: community,
+                  /// HOT 전체 리스트
+                  SizedBox(
+                    height: 270,
+                    child: ListView.separated(
+                      itemCount: hot.length,
+                      scrollDirection: Axis.horizontal,
+                      separatorBuilder: (_, __) {
+                        return const SizedBox(width: TSizes.sm);
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return GestureDetector(
+                          onTap: () {
+                            THelperFunctions.navigateToScreen(context, CommunityRecentScreen(model: hot[index]));
+                          },
+                          child: TCommunityAllHotCardWidget(
+                            containerWidth: 200,
+                            imageUrl: 'asset/img/car_image.jpeg',
+                            imageHeight: 200,
+                            nickName: hot[index].creator,
+                            title: hot[index].content,
+                            likeCount: hot[index].favorite.toString(),
+                          ),
                         );
-                      }),
-                ),]
-              ),
-            ),
-          ],
-        ),
-      ),
+                      },
+                    ),
+                  ),
+
+                  const SizedBox(height: TSizes.spaceBtwSections),
+
+                  /// 최신글
+                  /// 최신글 section heading
+                  const TCommunitySectionHeading(
+                    firstTitle: '최신글',
+                    secondTitle: '전체',
+                  ),
+
+                  const SizedBox(height: TSizes.spaceBtwItems),
+
+                  TCommunityAllLatestPostWidget(
+                    model: community,
+                  )
+                ],
+              );
+            }
+            return TCommunityAllLatestPostWidget(
+              model: community,
+            );
+          }),
     );
   }
 }
