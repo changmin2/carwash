0ac89b352434c11f1fb19fc938eae8c67af5daf4
rsy0921 <rsy0921@naver.com>
2024-01-31 05:08:57 +0900

    디자인 소스 정리

--- lib/community/view/community_register_screen.dart
+++ lib/community/view/community_register_screen.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
+import 'package:carwash/common/component/rounded_container.dart';
+import 'package:carwash/common/const/colors.dart';
 import 'package:carwash/common/const/sizes.dart';
 import 'package:carwash/common/layout/default_layout_v2.dart';
+import 'package:carwash/common/utils/helpers/helper_functions.dart';
 import 'package:carwash/community/model/requestRegister_model.dart';
 import 'package:carwash/community/repository/community_repository.dart';
 import 'package:carwash/community/view/community_screen.dart';
@@ -66,6 +69,7 @@ class _CommunityRegisterState extends ConsumerState<CommunityRegisterScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+
               /// 제목
               Row(
                 children: [
@@ -91,6 +95,7 @@ class _CommunityRegisterState extends ConsumerState<CommunityRegisterScreen> {
                   },
                 ),
               ),
+
               const SizedBox(height: TSizes.spaceBtwInputFields),
 
               /// 내용
@@ -115,7 +120,7 @@ class _CommunityRegisterState extends ConsumerState<CommunityRegisterScreen> {
                   },
                   decoration: const InputDecoration(hintText: '자유롭게 내용을 작성하세요.'),
                   keyboardType: TextInputType.multiline,
-                  maxLines: 8,
+                  maxLines: 6,
                 ),
               ),
               const SizedBox(height: TSizes.spaceBtwInputFields),
@@ -132,154 +137,149 @@ class _CommunityRegisterState extends ConsumerState<CommunityRegisterScreen> {
                   decoration: const InputDecoration(
                     hintText: '태그를 입력하세요.',
                   ),
-                  validator: (value){
-                    if(value!.isEmpty){
+                  validator: (value) {
+                    if (value!.isEmpty) {
                       return "태그를 하나 입력하세요.";
                     }
                   },
                 ),
               ),
+              const SizedBox(height: TSizes.spaceBtwInputFields),
 
-              const SizedBox(height: TSizes.spaceBtwSections),
+              /// 사진등록
+              Text('사진등록', style: Theme.of(context).textTheme.titleMedium),
+              const SizedBox(height: TSizes.spaceBtwInputFields / 2),
               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   //카메라로 촬영하기
-                  Container(
-                      margin: EdgeInsets.all(10),
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(color: Colors.lightBlueAccent, borderRadius: BorderRadius.circular(5),
-                        boxShadow: [
-                          BoxShadow(color: Colors.grey.withOpacity(0.5), spreadRadius: 0.5, blurRadius: 5)
-                        ],
+                  TRoundedContainer(
+                    padding: const EdgeInsets.all(TSizes.sm),
+                    backgroundColor: const Color(0xff2F80ED),
+                    child: IconButton(
+                      onPressed: () async {
+                        image = await picker.pickImage(source: ImageSource.camera);
+                        //카메라로 촬영하지 않고 뒤로가기 버튼을 누를 경우, null값이 저장되므로 if문을 통해 null이 아닐 경우에만 images변수로 저장하도록 합니다
+                        if (image != null) {
+                          if (images.length > 2) {
+                            THelperFunctions.showSnackBar(context, '사진은 3개까지 등록 할 수 있습니다.');
+                            // ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('사진은 3개까지 등록 할 수 있습니다.')));
+                          } else {
+                            setState(() {
+                              images.add(image);
+                            });
+                          }
+                        }
+                      },
+                      icon: const Icon(
+                        Icons.add_a_photo,
+                        size: 30,
+                        color: Colors.white,
                       ),
-                      child: IconButton(
-                          onPressed: () async {
-                            image = await picker.pickImage(source: ImageSource.camera);
-                            //카메라로 촬영하지 않고 뒤로가기 버튼을 누를 경우, null값이 저장되므로 if문을 통해 null이 아닐 경우에만 images변수로 저장하도록 합니다
-                            if (image != null) {
-                              if(images.length>2){
-                                ScaffoldMessenger.of(context).showSnackBar(SnackBar
-                                  (
-                                    content: Text('사진은 3개까지 등록 할 수 있습니다.')
-                                ));
-                              }else{
-                                setState(() {
-                                  images.add(image);
-                                });
-                              }
-                            }
-                          },
-                          icon: Icon(Icons.add_a_photo, size: 30, color: Colors.white,)
-                      )
+                    ),
                   ),
 
-                  Container(
-                      margin: EdgeInsets.all(10),
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(color: Colors.lightBlueAccent, borderRadius: BorderRadius.circular(5),
-                        boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), spreadRadius: 0.5, blurRadius: 5)],
+                  const SizedBox(width: TSizes.spaceBtwItems / 2),
+
+                  /// 갤러리에서 가져오기
+                  TRoundedContainer(
+                    padding: const EdgeInsets.all(TSizes.sm),
+                    backgroundColor: const Color(0xff2F80ED),
+                    child: IconButton(
+                      onPressed: () async {
+                        multiImage = await picker.pickMultiImage();
+                        if (multiImage.length > 3 || (multiImage.length - images.length).abs() > 3) {
+                          THelperFunctions.showSnackBar(context, '사진은 3개까지 등록 할 수 있습니다.');
+                        }
+                        setState(() {
+                          images.addAll(multiImage);
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.add_photo_alternate_outlined,
+                        size: 30,
+                        color: Colors.white,
                       ),
-                      child: IconButton(
-                          onPressed: () async {multiImage = await picker.pickMultiImage();
-                          if(multiImage.length>3 || (multiImage.length-images.length).abs()>3){
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar
-                              (
-                                content: Text('사진은 3개까지 등록 할 수 있습니다.')
-                            ));
-                          }
-                          setState(() {
-                              images.addAll(multiImage);
-                          });
-                          },
-                          icon: Icon(
-                            Icons.add_photo_alternate_outlined,
-                            size: 30,
-                            color: Colors.white,
-                          )
-                      )
+                    ),
                   ),
                 ],
               ),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: GridView.builder(padding: EdgeInsets.all(0),
-                  shrinkWrap: true,
-                  itemCount: images.length, //보여줄 item 개수. images 리스트 변수에 담겨있는 사진 수 만큼.
-                  gridDelegate:
-                  SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, //1 개의 행에 보여줄 사진 개수
-                    childAspectRatio:
-                    1 / 1, //사진 의 가로 세로의 비율
-                    mainAxisSpacing: 10, //수평 Padding
-                    crossAxisSpacing: 10, //수직 Padding
-                  ),
-                  itemBuilder: (BuildContext context, int index) {
-                    // 사진 오른 쪽 위 삭제 버튼을 표시하기 위해 Stack을 사용함
-                    return Stack(
-                      alignment: Alignment.topRight,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              image:
-                              DecorationImage(
-                                  fit: BoxFit.cover,  //사진 크기를 Container 크기에 맞게 조절
-                                  image: FileImage(File(images[index]!.path   // images 리스트 변수 안에 있는 사진들을 순서대로 표시함
-                                  ))
-                              )
+
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              /// 사진 목록
+              GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: images.length, //보여줄 item 개수. images 리스트 변수에 담겨있는 사진 수 만큼.
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, //1 개의 행에 보여줄 사진 개수
+                  childAspectRatio: 1 / 1, //사진 의 가로 세로의 비율
+                  mainAxisSpacing: 10, //수평 Padding
+                  crossAxisSpacing: 10, //수직 Padding
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  // 사진 오른 쪽 위 삭제 버튼을 표시하기 위해 Stack을 사용함
+                  return Stack(
+                    alignment: Alignment.topRight,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(TSizes.sm),
+                          image: DecorationImage(
+                            fit: BoxFit.cover, //사진 크기를 Container 크기에 맞게 조절
+                            image: FileImage(
+                              // images 리스트 변수 안에 있는 사진들을 순서대로 표시함
+                              File(images[index]!.path),
+                            ),
                           ),
                         ),
-                        Container(
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius:
-                              BorderRadius.circular(5),
-                            ),
-                            width:30,
-                            height:30,
-                            //삭제 버튼
-                            child: IconButton(
-                              padding: EdgeInsets.zero,
-                              constraints: BoxConstraints(),
-                              icon: Icon(Icons.close,
-                                  color: Colors.white,
-                                  size: 10),
-                              onPressed: () {
-                                //버튼을 누르면 해당 이미지가 삭제됨
-                                setState(() {
-                                  images.remove(images[index]);
-                                });
-                              },
-                            )
-                        )
-                      ],
-                    );
-                  },
-                ),
+                      ),
+                      TRoundedContainer(
+                        backgroundColor: Colors.black,
+                        radius: TSizes.xs,
+                        width: 24,
+                        height: 24,
+                        //삭제 버튼
+                        child: IconButton(
+                          padding: EdgeInsets.zero,
+                          constraints: const BoxConstraints(),
+                          icon: const Icon(
+                            Icons.close,
+                            color: Colors.white,
+                            size: 10,
+                          ),
+                          onPressed: () {
+                            //버튼을 누르면 해당 이미지가 삭제됨
+                            setState(() {
+                              images.remove(images[index]);
+                            });
+                          },
+                        ),
+                      )
+                    ],
+                  );
+                },
               ),
+
+              const SizedBox(height: TSizes.spaceBtwSections),
+
               /// 등록 버튼
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
                   onPressed: () async {
-                    if (_titleKey.currentState!.validate() && _contentKey.currentState!.validate()
-                          && _tagKey.currentState!.validate()) {
+                    if (_titleKey.currentState!.validate() && _contentKey.currentState!.validate() && _tagKey.currentState!.validate()) {
                       final user = ref.read(userMeProvider) as UserModel;
 
                       showModalBottomSheet(
                           context: context,
-                          builder: (BuildContext context){
-                            return Container(
+                          builder: (BuildContext context) {
+                            return const SizedBox(
                               height: 100,
-                              child: Center(
-                                  child: Text(
-                                      '업로드중...'
-                                  )
-                              ),
+                              child: Center(child: Text('업로드 중...')),
                             );
-                          }
-                      );
+                          });
 
                       await uploadImage();
 
@@ -289,7 +289,7 @@ class _CommunityRegisterState extends ConsumerState<CommunityRegisterScreen> {
                         title: title!,
                         category: category,
                         hastag: tag!,
-                        imgUrl: _downloadUrls == null ? '' : _downloadUrls.toString()!
+                        imgUrl: _downloadUrls == null ? '' : _downloadUrls.toString()!,
                       );
 
                       await ref.read(communityRepositoryProvider).register(param);
@@ -300,22 +300,21 @@ class _CommunityRegisterState extends ConsumerState<CommunityRegisterScreen> {
                   child: const Text('등록'),
                 ),
               ),
-
             ],
           ),
         ),
       ),
     );
   }
+
   Future<void> uploadImage() async {
     final now = DateTime.now();
-    if(images.length>0){
-      for(int i = 0; i <images.length; i++){
-        File _file  = File(images[i]!.path);
-        var ref = storage.ref().child('washRecord/$now'+i.toString()+'.png');
+    if (images.isNotEmpty) {
+      for (int i = 0; i < images.length; i++) {
+        File _file = File(images[i]!.path);
+        var ref = storage.ref().child('washRecord/$now' + i.toString() + '.png');
         await ref.putFile(_file);
         _downloadUrls.add(await ref.getDownloadURL());
-
       }
     }
   }
