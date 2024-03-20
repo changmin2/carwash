b167aa308f8867bfc83cb70ca2313500abd686be
rsy0921 <rsy0921@naver.com>
2024-03-20 23:41:51 +0900

    세차라이프 디자인 변경 팔로우 버튼 추가

--- lib/community/view/community_carwash_life/community_carwash_life_screen.dart
+++ lib/community/view/community_carwash_life/community_carwash_life_screen.dart
@@ -1,11 +1,10 @@
+import 'package:carwash/common/const/sizes.dart';
 import 'package:carwash/community/view/community_carwash_life/widget/comunity_carwash_life_list_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../common/view/pagination_list_view.dart';
 import '../../provider/communityProvider.dart';
-import '../community_detail_screen.dart';
 
 class TCommunityCarWashLifeScreen extends ConsumerWidget {
   const TCommunityCarWashLifeScreen({
@@ -13,24 +12,20 @@ class TCommunityCarWashLifeScreen extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
     ref.watch(communityProvider);
+
     return Padding(
-      padding: const EdgeInsets.only(top: 16),
+      padding: const EdgeInsets.only(top: TSizes.defalutSpace),
       child: PaginationListView(
-          provider: communityProvider,
-          itemBuilder: <CommunityModel>(_,index,community){
-            return GestureDetector(
-              onTap: (){
-                context.goNamed(CommunityDetailScreen.routeName,
-                    pathParameters: {'id':community.id.toString()});
-              },
-              child: TCommunityCarWashLifeListWidget(
-                model: community,
-              ),
-            );
-          }),
+        provider: communityProvider,
+        itemBuilder: <CommunityModel>(_, index, community) {
+          return TCommunityCarWashLifeListWidget(
+            model: community,
+          );
+        },
+      ),
     );
   }
 }
-
--- lib/community/view/community_carwash_life/widget/comunity_carwash_life_list_widget.dart
+++ lib/community/view/community_carwash_life/widget/comunity_carwash_life_list_widget.dart
@@ -1,6 +1,11 @@
+import 'package:carwash/common/const/colors.dart';
+import 'package:carwash/common/utils/helpers/helper_functions.dart';
+import 'package:carwash/community/view/community_detail_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../../../common/component/rounded_container.dart';
@@ -20,11 +25,11 @@ class TCommunityCarWashLifeListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<String> imgs = [];
-    if(model.imgUrls != ''){
+    if (model.imgUrls != '') {
       imgs = model.imgUrls!.split("[")[1].split("]")[0].split(",").toList();
     }
+
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         /// 유저사진, 닉네임, 날짜, 팔로우 버튼
         Padding(
@@ -32,9 +37,10 @@ class TCommunityCarWashLifeListWidget extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              /// 유저 사진
               Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  /// 유저 사진
                   const TRoundedImage(
                     width: 40,
                     height: 40,
@@ -42,6 +48,7 @@ class TCommunityCarWashLifeListWidget extends StatelessWidget {
                     imageUrl: 'asset/img/profile_image.png',
                     borderRadius: 100,
                   ),
+
                   const SizedBox(width: TSizes.spaceBtwItems / 2),
 
                   /// 닉네임 날짜
@@ -49,6 +56,7 @@ class TCommunityCarWashLifeListWidget extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(model.creator, style: Theme.of(context).textTheme.bodyMedium),
+                      const SizedBox(height: TSizes.xs),
                       Text(model.createDate.split("T")[0], style: Theme.of(context).textTheme.labelMedium),
                     ],
                   ),
@@ -56,16 +64,19 @@ class TCommunityCarWashLifeListWidget extends StatelessWidget {
               ),
 
               /// 팔로우 버튼
-              // TRoundedContainer(
-              //   padding: const EdgeInsets.all(TSizes.sm),
-              //   radius: 10,
-              //   showBorder: true,
-              //   backgroundColor: Colors.redAccent,
-              //   child: Text(
-              //     '팔로우',
-              //     style: Theme.of(context).textTheme.bodyMedium!.apply(color: Colors.white),
-              //   ),
-              // ),
+              GestureDetector(
+                onTap: () => THelperFunctions.showAlert(context, '', '업데이트 예정입니다.'),
+                child: TRoundedContainer(
+                  padding: const EdgeInsets.all(TSizes.sm),
+                  radius: 10,
+                  // showBorder: true,
+                  backgroundColor: Colors.indigoAccent,
+                  child: Text(
+                    '팔로우',
+                    style: Theme.of(context).textTheme.bodyMedium!.apply(color: Colors.white),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -73,97 +84,97 @@ class TCommunityCarWashLifeListWidget extends StatelessWidget {
         const SizedBox(height: TSizes.spaceBtwItems),
 
         /// 사진
-        imgs.length == 0
-        ?
-        Container(
-          height: 150,
-          child: Center(
-            child: Text(
-              'No Image'
-            ),
-          ),
-        )
-        :
-        SizedBox(
-          height: 350,
-          child: PageView.builder(
-            controller: controller,
-            itemCount: imgs.length,
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (_, int index) {
-              return TRoundedImage(
-                fit: BoxFit.cover,
-                imageUrl: imgs[index].trim(),
-                borderRadius: 0,
-                isNetworkImage: true,
-              );
-            },
-          ),
-        ),
-
-        const SizedBox(height: TSizes.spaceBtwItems),
-
-        imgs.length == 0
-        ?
-        Container()
-        :
-        Center(
-          child: SmoothPageIndicator(
-            controller: controller,
-            count: imgs.length,
-            effect: const ExpandingDotsEffect(
-              activeDotColor: Colors.black,
-              dotHeight: 6,
-            ),
-          ),
-        ),
-
-        const SizedBox(height: TSizes.spaceBtwItems),
-
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: TSizes.defalutSpace),
-          child: Row(
+        GestureDetector(
+          onTap: () => context.goNamed(CommunityDetailScreen.routeName, pathParameters: {'id': model.id.toString()}),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                '추천',
-                style: Theme.of(context).textTheme.bodySmall,
+              imgs.isEmpty
+                  ? const SizedBox(
+                      height: 150,
+                      child: Center(
+                        child: Text('No Image'),
+                      ),
+                    )
+                  : SizedBox(
+                      height: 350,
+                      child: PageView.builder(
+                        controller: controller,
+                        itemCount: imgs.length,
+                        physics: const BouncingScrollPhysics(),
+                        itemBuilder: (_, int index) {
+                          return TRoundedImage(
+                            fit: BoxFit.cover,
+                            imageUrl: imgs[index].trim(),
+                            borderRadius: 0,
+                            isNetworkImage: true,
+                          );
+                        },
+                      ),
+                    ),
+          
+              const SizedBox(height: TSizes.spaceBtwItems),
+          
+              imgs.isEmpty
+                  ? Container()
+                  : Center(
+                      child: SmoothPageIndicator(
+                        controller: controller,
+                        count: imgs.length,
+                        effect: const ExpandingDotsEffect(
+                          activeDotColor: Colors.black,
+                          dotHeight: 6,
+                        ),
+                      ),
+                    ),
+          
+              const SizedBox(height: TSizes.spaceBtwItems),
+          
+              /// 좋아요 and 댓글
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: TSizes.defalutSpace),
+                child: Row(
+                  children: [
+                    Text(
+                      '좋아요',
+                      style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Colors.redAccent),
+                    ),
+                    const SizedBox(width: TSizes.xs),
+                    Text(
+                      "${model.favorite}개",
+                      style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Colors.redAccent),
+                    ),
+                    const SizedBox(width: TSizes.spaceBtwItems),
+          
+                    Text(
+                      '댓글',
+                      style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Colors.blueAccent),
+                    ),
+                    const SizedBox(width: TSizes.xs),
+                    Text(
+                      '${model.commentCnt}개',
+                      style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Colors.blueAccent),
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(width: TSizes.xs),
-              Text(
-                model.favorite.toString() + "개",
-                style: Theme.of(context).textTheme.bodySmall,
+          
+              const SizedBox(height: TSizes.spaceBtwItems / 2),
+          
+              /// 작성글
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: TSizes.defalutSpace),
+                child: Text(
+                  model.content,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
-              const SizedBox(width: TSizes.spaceBtwItems / 2),
-
-              Text('|', style: Theme.of(context).textTheme.bodySmall),
-              const SizedBox(width: TSizes.spaceBtwItems / 2),
-
-              Text(
-                '댓글',
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
-              const SizedBox(width: TSizes.xs),
-
-              Text(
-                model.commentCnt.toString()+'개',
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
             ],
           ),
         ),
-
-        const SizedBox(height: TSizes.spaceBtwItems),
-
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: TSizes.defalutSpace),
-          child: Text(
-            model.content,
-            style: Theme.of(context).textTheme.bodyMedium,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-          ),
-        ),
-        SizedBox(height: 16)
       ],
     );
   }
