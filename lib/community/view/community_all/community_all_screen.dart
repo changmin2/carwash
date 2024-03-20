eb7adf63a26b3f00cb97a354452de7ea7314923b
rsy0921 <rsy0921@naver.com>
2024-03-20 22:19:10 +0900

    커뮤니티 전체 소스 정리

--- lib/community/view/community_all/community_all_screen.dart
+++ lib/community/view/community_all/community_all_screen.dart
@@ -26,81 +26,89 @@ class TCommunityAllScreen extends ConsumerWidget {
     return Padding(
       padding: const EdgeInsets.all(TSizes.defalutSpace),
       child: PaginationListView(
-          provider: communityProvider,
-          itemBuilder: <CommunityModel>(_, index, community) {
-            if (index == 0) {
-              return Column(
-                children: [
-                  /// HOT 전체
-                  /// HOT section header
-                  const TCommunitySectionHeading(
-                    firstTitle: 'HOT',
-                    firstTitleColor: Colors.red,
-                    secondTitle: '전체',
-                    // showActionButton: true,
-                    // buttonTitle: 'See All',
-                  ),
+        provider: communityProvider,
+        itemBuilder: <CommunityModel>(_, index, community) {
+          if (index == 0) {
+            return Column(
+              children: [
+                /// HOT 전체
+                /// HOT section header
+                const TCommunitySectionHeading(
+                  firstTitle: 'HOT',
+                  firstTitleColor: Colors.red,
+                  secondTitle: '전체',
+                  // showActionButton: true,
+                  // buttonTitle: 'See All',
+                ),
 
-                  const SizedBox(height: TSizes.spaceBtwItems),
-                  hot.length>0
-                  ?
-                  /// HOT 전체 리스트
-                  SizedBox(
-                    height: 270,
-                    child: ListView.separated(
-                      itemCount: hot.length >= 5 ? 5 : hot.length,
-                      scrollDirection: Axis.horizontal,
-                      separatorBuilder: (_, __) {
-                        return const SizedBox(width: TSizes.sm);
-                      },
-                      itemBuilder: (BuildContext context, int index) {
-                        var imgs = '';
-                        hot[index].imgUrls?.length != 0
-                        ?
-                            imgs = hot[index].imgUrls!.split('[')[1].split(']')[0].split(",")[0]
-                        : null;
+                const SizedBox(height: TSizes.spaceBtwItems),
 
-                        return GestureDetector(
-                          onTap: () {
-                            THelperFunctions.navigateToScreen(context, CommunityRecentScreen(model: hot[index],flag: 1,));
+                hot.length > 0
+                    ?
+
+                    /// HOT 전체 리스트
+                    SizedBox(
+                        height: 270,
+                        child: ListView.separated(
+                          itemCount: hot.length >= 5 ? 5 : hot.length,
+                          scrollDirection: Axis.horizontal,
+                          separatorBuilder: (_, __) {
+                            return const SizedBox(width: TSizes.sm);
                           },
-                          child: TCommunityAllHotCardWidget(
-                            containerWidth: 200,
-                            imageUrl: imgs == '' ? 'asset/img/no_image.png'
-                            : imgs,
-                            imageHeight: 200,
-                            isNetworkImage: imgs == '' ? false : true,
-                            nickName: hot[index].creator,
-                            title: hot[index].content,
-                            likeCount: hot[index].favorite.toString(),
-                            commentCnt: hot[index].commentCnt.toString(),
-                          ),
-                        );
-                      },
-                    ),
-                  )
-                  :Container(),
-                  const SizedBox(height: TSizes.spaceBtwSections),
+                          itemBuilder: (BuildContext context, int index) {
+                            var imgs = '';
+                            hot[index].imgUrls?.length != 0 ? imgs = hot[index].imgUrls!.split('[')[1].split(']')[0].split(",")[0] : null;
 
-                  /// 최신글
-                  /// 최신글 section heading
-                  const TCommunitySectionHeading(
-                    firstTitle: '최신글',
-                    secondTitle: '전체',
-                  ),
+                            return GestureDetector(
+                              behavior: HitTestBehavior.translucent,
+                              onTap: () {
+                                THelperFunctions.navigateToScreen(
+                                  context,
+                                  CommunityRecentScreen(
+                                    model: hot[index],
+                                    flag: 1,
+                                  ),
+                                );
+                              },
+                              child: TCommunityAllHotCardWidget(
+                                containerWidth: 200,
+                                imageUrl: imgs == '' ? 'asset/img/no_image.png' : imgs,
+                                imageHeight: 200,
+                                isNetworkImage: imgs == '' ? false : true,
+                                nickName: hot[index].creator,
+                                title: hot[index].content,
+                                likeCount: hot[index].favorite.toString(),
+                                commentCnt: hot[index].commentCnt.toString(),
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    : Container(),
 
-                  const SizedBox(height: TSizes.spaceBtwItems),
+                const SizedBox(height: TSizes.spaceBtwSections),
 
-                  TCommunityAllLatestPostWidget(
-                    model: community,
-                  )
-                ],
-              );
-            }
-            return TCommunityAllLatestPostWidget(
-              model: community,
+                /// 최신글
+                /// 최신글 section heading
+                const TCommunitySectionHeading(
+                  firstTitle: '최신글',
+                  secondTitle: '전체',
+                ),
+
+                const SizedBox(height: TSizes.spaceBtwItems),
+
+                TCommunityAllLatestPostWidget(
+                  model: community,
+                )
+              ],
             );
-          }),
+          }
+
+          return TCommunityAllLatestPostWidget(
+            model: community,
+          );
+        },
+      ),
     );
   }
 }
