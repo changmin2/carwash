29e740d971d661b2c175f52cfcb1bc41ea00092b
rsy0921 <rsy0921@naver.com>
2024-04-22 23:21:30 +0900

    날씨 디자인 변경

--- lib/common/component/weather.dart
+++ lib/common/component/weather.dart
@@ -13,105 +13,103 @@ class Weather extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final day = weatherInfo.dt_txt.split(" ")[0].split("-");
     final time = weatherInfo.dt_txt.split(" ")[1].split(":");
-    final week = DateFormat('E','ko_KR').format(DateTime.parse(day[0] +'-' + day[1] + '-' +day[2]));
+    final week = DateFormat('E', 'ko_KR').format(DateTime.parse('${day[0]}-${day[1]}-${day[2]}'));
 
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         /// 날짜
         Text(
-          '${day[1]}/${day[2]}(${week})',
+          '${day[1]}/${day[2]}($week)',
           style: Theme.of(context).textTheme.bodyMedium,
         ),
 
+        // Row(
+        //   children: [
+        //     const Icon(
+        //       Icons.water_drop_outlined,
+        //       size: 16,
+        //     ),
+        //
+        //     const SizedBox(
+        //       width: TSizes.xs,
+        //     ),
+        //
+        //     /// 습도인데 강수확률로 변경 요청
+        //     Text(
+        //       '${weatherInfo.humidity}%',
+        //       style: Theme.of(context).textTheme.bodyMedium,
+        //     ),
+        //   ],
+        // ),
+
         Row(
           children: [
-            const Icon(
-              Icons.water_drop_outlined,
-              size: 16,
-            ),
-
-            const SizedBox(
-              width: TSizes.xs,
-            ),
-
-            /// 습도인데 강수확률로 변경 요청
             Text(
-              '${weatherInfo.humidity}%',
+              '오전',
               style: Theme.of(context).textTheme.bodyMedium,
             ),
+
+            const SizedBox(width: TSizes.xs),
+
+            /// 오전 날씨 아이콘
+            weatherInfo.m_weather == 'Clear'
+                ? const Icon(
+                    Iconsax.sun_14,
+                    color: Colors.red,
+                    size: 24,
+                  )
+                : weatherInfo.m_weather == 'Clouds'
+                    ? const Icon(
+                        Iconsax.cloud,
+                        color: Colors.grey,
+                        size: 24,
+                      )
+                    : const Icon(
+                        Iconsax.cloud_drizzle,
+                        color: Colors.blue,
+                        size: 24,
+                      ),
+
+            const SizedBox(width: TSizes.md),
+
+            Text(
+              '오후',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+
+            const SizedBox(width: TSizes.xs),
+
+            /// 오후 6시 날씨 아이콘
+            weatherInfo.a_weather == 'Clear'
+                ? const Icon(
+                    Iconsax.sun_14,
+                    color: Colors.red,
+                    size: 24,
+                  )
+                : weatherInfo.a_weather == 'Clouds'
+                    ? const Icon(
+                        Iconsax.cloud,
+                        color: Colors.grey,
+                        size: 24,
+                      )
+                    : const Icon(
+                        Iconsax.cloud_drizzle,
+                        color: Colors.blue,
+                        size: 24,
+                      ),
           ],
         ),
 
-        /// 오전 날씨 아이콘
-        weatherInfo.m_weather == 'Clear'
-            ? const Icon(
-          Iconsax.sun_14,
-          color: Colors.red,
-          size: 24,
-        )
-            : weatherInfo.m_weather == 'Clouds'
-            ? const Icon(
-          Iconsax.cloud,
-          color: Colors.grey,
-          size: 24,
-        )
-            : const Icon(
-          Iconsax.cloud_drizzle,
-          color: Colors.blue,
-          size: 24,
+        /// 최고 온도 / 최저 온도
+        Text(
+          '${(weatherInfo.temp_max - 273.15).floor()}℃ / ${(weatherInfo.temp_min - 273.15).floor()}℃',
+          style: Theme.of(context).textTheme.bodyMedium,
         ),
 
 
-        /// 오후 6시 날씨 아이콘
-        weatherInfo.a_weather == 'Clear'
-            ? const Icon(
-                Iconsax.sun_14,
-                color: Colors.red,
-                size: 24,
-              )
-            : weatherInfo.a_weather == 'Clouds'
-                ? const Icon(
-                    Iconsax.cloud,
-                    color: Colors.grey,
-                    size: 24,
-                  )
-                : const Icon(
-                    Iconsax.cloud_drizzle,
-                    color: Colors.blue,
-                    size: 24,
-                  ),
-
-        /// 최저 온도
-        SizedBox(
-          width: 30,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                '${(weatherInfo.temp_min - 273.15).floor()}℃',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ],
-          ),
-        ),
-
-        /// 최고 온도
-        SizedBox(
-          width: 30,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                '${(weatherInfo.temp_max - 273.15).floor()}℃',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ],
-          ),
-        ),
       ],
     );
   }
