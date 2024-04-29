af953680b26b0b92ef641f67ef17ea2b96b80f76
changmin2 <dlckdals9467@naver.com>
2024-04-29 23:50:51 +0900

    Event fix

--- lib/common/const/data.dart
+++ lib/common/const/data.dart
@@ -8,8 +8,8 @@ final emulatorIp = '10.0.2.2:8080';
 //시뮬레이터 로컬
 final simulatorIp = '127.0.0.1:8080';
 //아이폰일 경우 시뮬레이터 아이피 아닐 경우 애뮬레이터 아이피
-final ip = Platform.isIOS ? simulatorIp : emulatorIp;
-//final ip = '49.50.165.212:8080';
+//final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+final ip = '49.50.165.212:8080';
 
 final openWeatherApiKey = 'fe7b80f0025e7127919bfc3ee7e1d45f';
 
